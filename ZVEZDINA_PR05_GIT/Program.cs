47242454975fbde7cbebc09f11b09161a91b90b7
Zvezdina <vika.zvezdina.06@mail.ru>
2025-02-07 11:18:08 +0300

    добавлена новая форма AddForm

--- ZVEZDINA_PR05_GIT/Program.cs
+++ ZVEZDINA_PR05_GIT/Program.cs
@@ -16,7 +16,7 @@ namespace ZVEZDINA_PR05_GIT
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new AddForm());
         }
     }
 }
